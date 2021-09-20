acef13464ca600437d51ee6d40b1279b4ad247e1
Neerajmaurya27 <mauryan074@gmail.com>
2021-09-20 16:23:56 +0530

    add sql

--- communityproject.sql
+++ communityproject.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 17, 2021 at 09:09 AM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 8.0.10
+-- Generation Time: Sep 20, 2021 at 12:52 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 7.3.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -39,7 +39,8 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`admin_id`, `username`, `email`, `password`) VALUES
-(1, 'anurag', 'anurag@gmail.com', '12345');
+(1, 'admin', 'admin@gmail.com', 'admin'),
+(2, 'vkgupta', 'vkgupta@gmail.com', 'vkgupta');
 
 -- --------------------------------------------------------
 
@@ -71,11 +72,10 @@ CREATE TABLE `child` (
 --
 
 INSERT INTO `child` (`id`, `child_Name`, `child_email`, `gender`, `isMarriageable`, `age`, `education`, `degree`, `profession`, `height`, `dateofbirth`, `faceofcomplexion`, `manglik`, `expectation`, `profile_pic`, `status`) VALUES
-(1, 'aamir ', 'khanboy9911998319@gmail.com', 'Male', '', 22, 'btech@gd.in', 'btech', 'btech', 10, '1999-03-30', 'btech', 'btech', 'csccjkdskknds sqkjakja,56645', '', ''),
-(2, 'xsmns', 'amanbhatia@gmail.com', 'Male', 'no', 22, 'btech@gd.in', 'bcom', 'bcom', 10, '2002-03-05', 'btech', 'btech', 'dsdddzdzzd', '', ''),
-(3, 'sksk', 'asj@gmail.com', 'Male', 'yes', 23, 'bsc', 'btech', 'mca', 15, '1698-03-20', 'mca', 'other', 'dgvxfdxfbxbxbx dsjhzjkz odkzlkzfds', 'IMG_20210203_111700_5142109801', ''),
-(4, 'dsgrt', 'ak114@gmai.com', 'Male', '', 0, 'bba', 'bcom', 'other', 7, '2021-09-24', 'btech', 'btech', 'sdfghh', '', ''),
-(5, 'dsgrt', 'ak114@gmai.com', 'Male', '', 0, 'bba', 'bcom', 'other', 7, '2021-09-24', 'btech', 'btech', 'sdfghh', '', '');
+(16, '12aamir ', 'aamir@gmail.com', 'Female', 'yes', 12, '12aamir ', 'bcom', 'bcom', 0, '0000-00-00', 'bcom', 'bcom', '', '', ''),
+(17, '12aamir ', 'aamir@gmail.com', 'Female', 'yes', 12, '12aamir ', 'bcom', 'bcom', 0, '0000-00-00', 'bcom', 'bcom', '', '', ''),
+(20, 'dummy', 'child@gmnail.cimn', 'Male', 'yes', 0, 'bca', 'btech', 'bcom', 7, '2021-09-13', 'bcom', 'bcom', '45', '', ''),
+(21, 'dummy don', 'site88@gmnail.cimn', 'Male', 'yes', 56, 'bca', 'btech', 'mca', 7, '2021-10-01', 'btech', 'mca', 'hj', '614736768a081.png', '');
 
 -- --------------------------------------------------------
 
@@ -101,17 +101,22 @@ CREATE TABLE `members` (
   `recieptNo` int(12) NOT NULL,
   `profilepic` varchar(255) NOT NULL,
   `status` int(10) NOT NULL DEFAULT 0,
-  `isLifeMember` int(10) NOT NULL DEFAULT 0
+  `isLifeMember` int(10) NOT NULL DEFAULT 0,
+  `pincode` int(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `members`
 --
 
-INSERT INTO `members` (`id`, `firstName`, `lastname`, `mobileNo`, `email`, `dob`, `gender`, `state`, `city`, `address`, `password`, `fatherName`, `motherName`, `lifeMemberNo`, `recieptNo`, `profilepic`, `status`, `isLifeMember`) VALUES
-(24, 'anurag', 'chindaliya', 2147483647, 'ak114@gmai.com', '2021-09-17', 'Male', 'haryana', 'ak114@gmai.com', 'jawahar colong', '12345', 'asdfg', 'sdfggg', 123456, 12345, '0%-FINANCE.jpg', 0, 0),
-(26, 'neeraj', 'chindaliya', 2147483647, 'ak114@gmai.com', '2021-09-17', 'Male', 'haryana', 'ak114@gmai.com', 'jawahar colong', '12345', 'asdfg', 'sdfggg', 123456, 12345, '0%-FINANCE.jpg', 1, 1),
-(27, 'dummy98', 'chauhan', 2147483647, 'new@gmail.com', '2021-08-30', 'Male', 'haryana', 'faridabad', 'ak114@gmai.com', '12345', 'asdfg', 'hsdjfh', 23456, 12345, 'K23.jpg', 0, 0);
+INSERT INTO `members` (`id`, `firstName`, `lastname`, `mobileNo`, `email`, `dob`, `gender`, `state`, `city`, `address`, `password`, `fatherName`, `motherName`, `lifeMemberNo`, `recieptNo`, `profilepic`, `status`, `isLifeMember`, `pincode`) VALUES
+(66, 'dummy925', 'jkl', 2147483647, 'anurag@gmail.com', '2021-09-28', 'Male', 'Puducherry', 'dfghj', '770, gali 1, Nagla Road NIT Faridabad, Haryana 121005', '12345', 'dummy name1', 'jk,', 32145, 123456, '61474d15b2ed8.jpg', 0, 0, 121),
+(67, 'newGender', 'newlastName', 4567890, 'anurag@gmail.com', '2021-09-10', 'Male', 'Daman and Diu', 'fghj', '770, gali 1, Nagla Road NIT Faridabad, Haryana 121005', '12345', 'ghjkl', 'ghjkl', 56789, 45678, '61470664c2bcb.png', 0, 0, 1210078),
+(68, 'dummy99', 'sdfasd', 88456789, 'anurag@gmail.com', '2021-09-18', 'Female', 'Andhra Pradesh', 'Faridabad', 'fde', '12345', 'dummy name', 'PHOOL', 0, 0, '61471d90d74c0.jpg', 0, 0, 121001),
+(69, 'abc', 'def', 1234567890, 'abc@gmail.com', '1996-06-19', 'Male', 'Haryana', 'Faridabad', 'qwertyu', '12345', 'dummy', 'dummyyy', 12345, 12345, '614743488a8ff.jpg', 0, 0, 121005),
+(70, 'dummy100', 'mas', 88456789, 'anurag@gmail.com', '1999-12-29', 'Female', 'Puducherry', 'dfghj', '770, gali 1, Nagla Road NIT Faridabad, Haryana 121005', '12345', 'dummyfathernamedumdu', 'dummymothernamedumdu', 2345, 1234, '6148511c53a84.png', 0, 0, 121006),
+(71, 'neeraj', 'maurya', 12345678, 'dgjfas@gahjl.com', '2021-09-01', 'm', 'haryana', 'faridabad', 'asd', 'a3s4d5fgyu', 'asdfgh', 'zxcvbn', 3456789, 987654, '', 0, 0, 121005),
+(72, 'dummy101', 'dummy last101', 2147483647, 'anurag@gmail.com', '1999-12-30', 'Female', 'Delhi', 'Faridabad', '770, gali 1, Nagla Road NIT Faridabad, Haryana 121005', '12345', 'Dummy Father Name101', 'dummy mother name101', 1250, 1201, '61485a9467c2c.png', 0, 0, 121005);
 
 --
 -- Indexes for dumped tables
@@ -143,19 +148,19 @@ ALTER TABLE `members`
 -- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `child`
 --
 ALTER TABLE `child`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `members`
 --
 ALTER TABLE `members`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=73;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
