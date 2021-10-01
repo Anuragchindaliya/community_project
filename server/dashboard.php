4d5c2655d3fdc8b7fecd420b33ac1f6d026e03f7
Neerajmaurya27 <mauryan074@gmail.com>
2021-10-01 17:37:45 +0530

    add catousel

--- server/dashboard.php
+++ server/dashboard.php
@@ -102,35 +102,13 @@ include "../includes/sidebar.php";
           <section class="col-lg-7 connectedSortable">
             <!-- Custom tabs (Charts with tabs)-->
             <div class="card">
-              <div class="card-header">
-                <h3 class="card-title">
-                  <i class="fas fa-chart-pie mr-1"></i>
-                  Sales
-                </h3>
-                <div class="card-tools">
-                  <ul class="nav nav-pills ml-auto">
-                    <li class="nav-item">
-                      <a class="nav-link active" href="#revenue-chart" data-toggle="tab">Area</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="#sales-chart" data-toggle="tab">Donut</a>
-                    </li>
-                  </ul>
-                </div>
-              </div><!-- /.card-header -->
-              <div class="card-body">
-                <div class="tab-content p-0">
-                  <!-- Morris chart - Sales -->
-                  <div class="chart tab-pane active" id="revenue-chart"
-                       style="position: relative; height: 300px;">
-                      <canvas id="revenue-chart-canvas" height="300" style="height: 300px;"></canvas>
-                   </div>
-                  <div class="chart tab-pane" id="sales-chart" style="position: relative; height: 300px;">
-                    <canvas id="sales-chart-canvas" height="300" style="height: 300px;"></canvas>
-                  </div>
-                </div>
-              </div><!-- /.card-body -->
+              <img class="life_memberImages"
+              src="../assets/image/img1.jpg" alt="">
             </div>
+             <div class="button_action">
+              <button class="btn_prev" onclick="lifeMemberCarousel()"><</button>
+              <button class="btn_next" onclick="lifeMemberCarouselNext()">></button>
+            </div> 
             <!-- /.card -->
           </section>
           <!-- /.Left col -->
@@ -139,25 +117,8 @@ include "../includes/sidebar.php";
 
             <!-- Map card -->
             <div class="card bg-gradient-primary">
-              <div class="card-header border-0">
-                <h3 class="card-title">
-                  <i class="fas fa-map-marker-alt mr-1"></i>
-                  Visitors
-                </h3>
-                <!-- card tools -->
-                <div class="card-tools">
-                  <button type="button" class="btn btn-primary btn-sm daterange" title="Date range">
-                    <i class="far fa-calendar-alt"></i>
-                  </button>
-                  <button type="button" class="btn btn-primary btn-sm" data-card-widget="collapse" title="Collapse">
-                    <i class="fas fa-minus"></i>
-                  </button>
-                </div>
-                <!-- /.card-tools -->
-              </div>
-              <div class="card-body">
-                <div id="world-map" style="height: 250px; width: 100%;"></div>
-              </div>
+              
+
               <!-- /.card-body-->
               <div class="card-footer bg-transparent">
                 <div class="row">
@@ -191,6 +152,33 @@ include "../includes/sidebar.php";
     </section>
     <!-- /.content -->
   </div>
+<script>
+  var btn_prev = document.querySelector(".btn_prev");
+  var btn_prev = document.querySelector(".btn_next");
+
+  var i = 0;
+  var LMI = ["img1.jpg","img2.jpg","img3.jpg"];
+  var carouseImage = document.querySelector(".life_memberImages");
+  function lifeMemberCarousel () {
+    carouseImage.src = `../assets/image/${LMI[i]}`
+    if ( i>0){
+      i--;
+    } else{
+      i=LMI.length-1;
+    }
+}
+function lifeMemberCarouselNext () {
+carouseImage.src = `../assets/image/${LMI[i]}`
+if ( i<LMI.length-1){
+      i++;
+    } else{;
+      i=0
+    }
+}
+    setInterval(lifeMemberCarouselNext, 2000);
+    window.onload(lifeMemberCarouselNext);
+  // btn_prev.addEventListener('click',lifeMemberCarousel);
+</script>
 <?php
 
 include "../includes/footer.php";
